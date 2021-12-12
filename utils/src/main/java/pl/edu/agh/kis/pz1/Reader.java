8cff0f9a2fdcaa321785c61c66e4758c31b69c89
Michal Strzezon <mstrzezon@gmail.com>
2021-12-12 18:47:06 +0100

    delete_unused_fields

--- utils/src/main/java/pl/edu/agh/kis/pz1/Reader.java
+++ utils/src/main/java/pl/edu/agh/kis/pz1/Reader.java
@@ -5,23 +5,46 @@ import org.apache.logging.log4j.LogManager;
 
 /**
  * Class representing the reader.
+ * The reader object encapsulates name and wants to enter to the reading room
+ * to read books.
+ *
+ * @author Michał Strzeżoń
  */
 public class Reader extends Thread{
+    /**
+     * the reading room the reader enters.
+     */
     private final ReadingRoom readingRoom;
+    /**
+     * name of the reader.
+     */
     private final String name;
-    String textRead;
-    String newText;
+    /**
+     * number of read books by the reader.
+     */
     int numberOfReadBooks;
     private static final Logger logger = LogManager.getLogger(Reader.class);
 
+    /**
+     * Creates the reader object.
+     * @param r reading room the reader will be enter.
+     * @param n name of the reader.
+     */
     public Reader(ReadingRoom r, String n) {
         this.readingRoom = r;
         this.name = n;
-        this.textRead = "";
-        this.newText = "";
         this.numberOfReadBooks = 0;
     }
 
+    /**
+     * Starts the thread.
+     * Reader:
+     * <ul>
+     *     <li>wants to enter to the reading room,</li>
+     *     <li>is reading the book,</li>
+     *     <li>is ending reading.</li>
+     * </ul>
+     */
     @Override
     public void run() {
         while (true) {
