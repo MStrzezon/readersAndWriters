3614855236bce3cfbcbd5186543913f3153e5d67
Michal Strzezon <mstrzezon@gmail.com>
2022-01-03 16:25:39 +0100

    delete_duplicates

--- main/src/main/java/pl/edu/agh/kis/pz1/Main.java
+++ main/src/main/java/pl/edu/agh/kis/pz1/Main.java
@@ -1,5 +1,8 @@
 package pl.edu.agh.kis.pz1;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Main class!
  */
@@ -7,30 +10,16 @@ public class Main
 {
     public static void main(String[] args) throws InterruptedException {
         ReadingRoom readingRoom = new ReadingRoom();
-        new Writer(readingRoom, "Jan Kochanowski").start();
-        Thread.sleep(10);
-        new Writer(readingRoom, "Adam Mickiewicz").start();
-        Thread.sleep(10);
-        new Writer(readingRoom, "Juliusz Slowacki").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Adam").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Krzysiek").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Michal").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Wojtek").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Julek").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Marcin").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Zygfryt").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Kacper").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Karolina").start();
-        Thread.sleep(10);
-        new Reader(readingRoom, "Martyna").start();
+        List<String> writersNames = Arrays.asList("Jan Kochanowski", "Adam Mickiewicz", "Juliusz Slowacki");
+        List<String> readersNames = Arrays.asList("Adam", "Krzysiek", "Michal", "Wojtek", "Julek", "Marcin", "Zygfryt",
+                                                    "Kacper", "Karolina", "Martyna");
+        for (String name : writersNames) {
+            new Writer(readingRoom, name).start();
+            Thread.sleep(10);
+        }
+        for (String name : readersNames) {
+            new Reader(readingRoom, name).start();
+            Thread.sleep(10);
+        }
     }
 }
