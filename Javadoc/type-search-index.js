f9754a76a9ba8bc1292cc29f90a9f86a129c6b5c
Michal Strzezon <mstrzezon@gmail.com>
2022-01-03 15:30:54 +0100

    delete_docs

--- Javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"pl.edu.agh.kis.pz1","l":"Main"},{"p":"pl.edu.agh.kis.pz1","l":"Reader"},{"p":"pl.edu.agh.kis.pz1","l":"ReadingRoom"},{"p":"pl.edu.agh.kis.pz1","l":"Writer"}];updateSearchResults();
