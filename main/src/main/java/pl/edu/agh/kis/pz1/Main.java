d840b38bd1c24ec65f63fa82b28605d19d28828a
Michal Strzezon <mstrzezon@gmail.com>
2022-01-03 13:49:09 +0100

    add_sleep

--- main/src/main/java/pl/edu/agh/kis/pz1/Main.java
+++ main/src/main/java/pl/edu/agh/kis/pz1/Main.java
@@ -6,20 +6,32 @@ package pl.edu.agh.kis.pz1;
  */
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ReadingRoom readingRoom = new ReadingRoom();
         new Writer(readingRoom, "Jan Kochanowski").start();
-        new Reader(readingRoom, "Adam").start();
-        new Reader(readingRoom, "Krzysiek").start();
-        new Reader(readingRoom, "Michal").start();
+        Thread.sleep(10);
         new Writer(readingRoom, "Adam Mickiewicz").start();
+        Thread.sleep(10);
         new Writer(readingRoom, "Juliusz Slowacki").start();
+        Thread.sleep(10);
+        new Reader(readingRoom, "Adam").start();
+        Thread.sleep(10);
+        new Reader(readingRoom, "Krzysiek").start();
+        Thread.sleep(10);
+        new Reader(readingRoom, "Michal").start();
+        Thread.sleep(10);
         new Reader(readingRoom, "Wojtek").start();
+        Thread.sleep(10);
         new Reader(readingRoom, "Julek").start();
+        Thread.sleep(10);
         new Reader(readingRoom, "Marcin").start();
+        Thread.sleep(10);
         new Reader(readingRoom, "Zygfryt").start();
+        Thread.sleep(10);
         new Reader(readingRoom, "Kacper").start();
+        Thread.sleep(10);
         new Reader(readingRoom, "Karolina").start();
+        Thread.sleep(10);
         new Reader(readingRoom, "Martyna").start();
     }
 }
